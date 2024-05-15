1da2344deb1660bc8f54b78057219e3fcba1515d
Tim-Hedlund <tim.hedlund@elev.ga.ntig.se>
2024-05-15 10:12:50 +0200

    Startat

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Client {
+import java.io.*;
+import java.net.*;
+public class Client {
+
+    public static void main(String[] args) {
+
+        Socket client = null;
+
+        int portNumber = 55621; //samma som andra
+        if (args.length >= 1) {
+            portNumber = Integer.parseInt(args[0]);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                String msg = "";
+
+                client = new Socket(InetAddress.getLocalHost(), portNumber);
+                System.out.println("Client socket is created " + client);
+
+                OutputStream clientOut = client.getOutputStream();
+                PrintWriter pw = new PrintWriter(clientOut, true);
+
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+                System.out.println("Enter your name. Type bye to exit. ");
+
+                msg = stdIn.readLine().trim();
+                pw.println(msg);
+
+                System.out.println("Message returned from the server = " + br.readLine());
+
+                pw.close();
+                br.close();
+                client.close();
+
+                if (msg.equalsIgnoreCase("bye")) {
+                    break;
+                }
+
+            } catch (IOException ie) {
+                System.out.println("I/O error " + ie);
+            }
+        }
+
+    }
+
 }
